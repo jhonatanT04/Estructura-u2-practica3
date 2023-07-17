a0475ab5cd506ca0bd817b128fa9548108898ca0
V3no Tacu <venotacu@gmail.com>
2023-07-17 09:38:37 -0500

    first commit\

---
+++ src/main/java/ec/edu/ups/practica/estructura/Nodo.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.practica.estructura;
+
+/**
+ *
+ * @author venot
+ */
+public class Nodo {
+    private Nodo izquierdo;
+    private Nodo derecho;
+    private Contacto contacto;
+
+    public Nodo(Contacto contacto) {
+        this.contacto = contacto;
+        this.derecho= null;
+        this.izquierdo= null;
+    }
+
+    public Nodo getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(Nodo izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public Nodo getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(Nodo derecho) {
+        this.derecho = derecho;
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+    
+    
+    
+}
